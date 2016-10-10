8c4a1bb76ce91190cdaa70938805f5d689dee5d7
Kazuki Ginnan <ginnan.kazuki@ist.osaka-u.ac.jp>
2016-10-10 17:02:22 +0900

    課題3コード追加

---
+++ hello_trema.rb
@@ -0,0 +1,12 @@
+# Hello World!
+
+
+class HelloTrema < Trema::Controller
+  def start(_args)
+    logger.info "#{self.name} started."
+  end
+
+  def switch_ready(datapath_id)
+    logger.info "Hello #{datapath_id.to_hex}!"
+  end
+end
